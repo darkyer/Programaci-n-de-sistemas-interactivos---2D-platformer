3d13dc0965bea0e13267e0d55cc669f715b2577c
Ernesto <ernesto.solares.guerrero@gmail.com>
2020-04-01 19:02:29 -0600

    initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float movX = 0;
+    float movY = 0;
+    Vector2 dir;
+    public float speed = 1;
+    public float jumpForce = 1;
+    public bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movX = Input.GetAxis("Horizontal");
+        movY = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+        dir = new Vector2(movX, movY);
+
+        Move(dir);
+    }
+
+
+    private void Jump()
+    {
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    private void Move(Vector2 dir)
+    {
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+}
